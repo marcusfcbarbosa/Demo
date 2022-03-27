2cff1c807326485901158d08cbf7699edad473ab
Marcus Fernando Correa Barboxz <marcusfcbarbosa@hotmail.com>
2022-03-27 19:23:27 -0300

    Migrations working

---
+++ 3-Infra/Demo.Infra/Migrations/20220327222128_FirstMigration.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Demo.Infra.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    Price = table.Column<decimal>(type: "decimal", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2022, 3, 27, 19, 21, 27, 415, DateTimeKind.Local).AddTicks(1686)),
+                    updateAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    role = table.Column<int>(type: "int", maxLength: 100, nullable: false),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    socialSecurityNumnber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    address = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2022, 3, 27, 19, 21, 27, 401, DateTimeKind.Local).AddTicks(3021)),
+                    updateAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_Order",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    deliveryAddress = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2022, 3, 27, 19, 21, 27, 423, DateTimeKind.Local).AddTicks(7400)),
+                    updateAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_Order", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_Order_tb_Product_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tb_Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_Order_tb_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "tb_User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_UserLogin",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    userId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    login = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    createdAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValue: new DateTime(2022, 3, 27, 19, 21, 27, 417, DateTimeKind.Local).AddTicks(6905)),
+                    updateAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_UserLogin", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_tb_UserLogin_tb_User_userId",
+                        column: x => x.userId,
+                        principalTable: "tb_User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_Order_UserId",
+                table: "tb_Order",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_UserLogin_userId",
+                table: "tb_UserLogin",
+                column: "userId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_Order");
+
+            migrationBuilder.DropTable(
+                name: "tb_UserLogin");
+
+            migrationBuilder.DropTable(
+                name: "tb_Product");
+
+            migrationBuilder.DropTable(
+                name: "tb_User");
+        }
+    }
+}
